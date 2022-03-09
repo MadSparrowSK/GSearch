6c83ea2b79a6948e973c6ee7e0ea12dbe8b4b7d7
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2022-03-09 20:14:24 +0300

    add sse realize hot-post mechanics and make post unique

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,30 @@
 import './App.css';
+import {useEffect, useRef, useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [hotPosts, setHotPosts] = useState([]);
+
+  useEffect(()=> {
+      subscribe();
+  }, [])
+
+  const subscribe = async () => {
+     /* const response = await axios.get('http://localhost:5000/api/posts');
+      console.log(response)*/
+      const post =  new EventSource('http://localhost:5000/api/hot-posts');
+      post.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          console.log(data)
+      }
+      post.onopen = (e) => {
+          console.log(e)
+      }
+      post.onerror = (e) => {
+          console.log(e)
+      }
+  }
+
   return (
     <div>
     </div>
